8fb4cbefd237058ea9fc7a8c7fbd66770d993709
Bryce Darling <bryce@brycedarling.com>
2017-09-05 18:59:16 -0400

    Add all current currency pairs

--- dist/currencyPairs.js
+++ dist/currencyPairs.js
@@ -1,11 +1,22 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-// TODO: add all other currency pairs
-exports.default = {
-  USDT_BTC: true,
-  USDT_ETH: true,
-  USDT_LTC: true
+var markets = {
+  BTC: ['AMP', 'ARDR', 'BCH', 'BCN', 'BCY', 'BELA', 'BLK', 'BTCD', 'BTM', 'BTS', 'BURST', 'CLAM', 'DASH', 'DCR', 'DGB', 'DOGE', 'EMC2', 'ETC', 'ETH', 'EXP', 'FCT', 'FLDC', 'FLO', 'GAME', 'GNO', 'GNT', 'GRC', 'HUC', 'LBC', 'LSK', 'LTC', 'MAID', 'NAUT', 'NAV', 'NEOS', 'NMC', 'NOTE', 'NXC', 'NXT', 'OMNI', 'PASC', 'PINK', 'POT', 'PPC', 'PPC', 'RADS', 'REP', 'RIC', 'SBD', 'SC', 'SJCX', 'STEEM', 'STR', 'STRAT', 'SYS', 'VIA', 'VRC', 'VTC', 'XBC', 'XCP', 'XEM', 'XMR', 'XPM', 'XRP', 'XVC', 'ZEC', 'ZRX'],
+  ETH: ['BCH', 'ETC', 'GNO', 'GNT', 'LSK', 'REP', 'STEEM', 'ZEC', 'ZRX'],
+  XMR: ['BCN', 'BLK', 'BTCD', 'DASH', 'LTC', 'MAID', 'NXT', 'ZEC'],
+  USDT: ['BCH', 'BTC', 'DASH', 'ETC', 'ETH', 'LTC', 'NXT', 'REP', 'STR', 'XMR', 'XRP', 'ZEC']
 };
+
+var currencyPairs = {};
+
+Object.keys(markets).forEach(function (market) {
+  var currencies = markets[market];
+  currencies.forEach(function (currency) {
+    currencyPairs[market + '_' + currency] = true;
+  });
+});
+
+exports.default = currencyPairs;
