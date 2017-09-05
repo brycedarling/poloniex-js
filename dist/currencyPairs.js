1fa57c13d00436b71a74e7cf5290be0c860d4ba4
Bryce Darling <bryce@brycedarling.com>
2017-09-05 14:24:31 -0400

    Refactored currency pairs to their own file

---
+++ dist/currencyPairs.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// TODO: add all other currency pairs
+exports.default = {
+  USDT_BTC: true,
+  USDT_ETH: true,
+  USDT_LTC: true
+};
---
+++ src/currencyPairs.js
@@ -0,0 +1,6 @@
+// TODO: add all other currency pairs
+export default {
+  USDT_BTC: true,
+  USDT_ETH: true,
+  USDT_LTC: true,
+};
