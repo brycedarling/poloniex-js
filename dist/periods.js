118ec007fe3c830b116dc833a91b4906533fd7c0
Bryce Darling <bryce@brycedarling.com>
2017-09-05 14:25:21 -0400

    Refactored time periods in to their own file

---
+++ dist/periods.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  300: true,
+  900: true,
+  1800: true,
+  7200: true,
+  14400: true,
+  86400: true
+};
---
+++ src/periods.js
@@ -0,0 +1,8 @@
+export default {
+  300: true,
+  900: true,
+  1800: true,
+  7200: true,
+  14400: true,
+  86400: true,
+};
