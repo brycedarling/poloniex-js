12e76bf47aed95a87fb62071fd84ff8d8c9e8077
Bryce Darling <bryce@brycedarling.com>
2017-09-29 14:32:54 -0400

    Return period keys for values

--- dist/periods.js
+++ dist/periods.js
@@ -4,10 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = {
-  300: true,
-  900: true,
-  1800: true,
-  7200: true,
-  14400: true,
-  86400: true
+  300: 300,
+  900: 900,
+  1800: 1800,
+  7200: 7200,
+  14400: 14400,
+  86400: 86400
 };
